35f75a2ce6be93ea218938a6672733e8b76e11ec
unknown <se092798@gmail.com>
2025-09-26 23:33:53 +0500

    novel code

---
+++ static/assets/js/hello.ts
@@ -0,0 +1,10 @@
+// demo.ts
+// Bu fayl faqat GitHub uchun qo‘shilgan, loyiha uni ishlatmaydi.
+
+// Oddiy TypeScript misol
+function greet(name: string): string {
+    return `Hello, ${name}!`;
+}
+
+const user = "Django Developer";
+console.log(greet(user));
