29695bba470af41180aef0a148728ad843e786d7
unknown <se092798@gmail.com>
2025-09-26 11:15:57 +0500

    start platforma

---
+++ fronend/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'index.html')
+def login(request):
+    return render(request, 'login.html')
+def register (request):
+    return render(request, 'register.html')
+
+def contact(request):
+    return render(request, 'contact.html')
